bf26e829b4fb54cce38c0dfe3f41bd618c62d597
saeedAliakbary <saeedaliakbari.123@gmail.com>
2023-10-07 13:29:03 +0330

    test-ApiRelay: third step of TDD create test for controller and failed !

---
+++ src/test/java/com/relay/apirelay/WeatherControllerTest.java
@@ -0,0 +1,44 @@
+package com.relay.apirelay;
+
+
+
+import com.relay.apirelay.service.WeatherService;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+class WeatherControllerTest {
+
+    @Mock
+    private WeatherService weatherService;
+
+    @InjectMocks
+    private WeatherController weatherController;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    void testGetWeather() {
+        when(weatherService.getWeather("Tehran")).thenReturn("Weather information for Tehran");
+        String response = weatherController.getWeather("Tehran");
+        assertEquals("Weather information for Tehran", response);
+    }
+
+    @Test
+    void testPostWeather() {
+        WeatherRequest request = new WeatherRequest("Paris", 48.8566, 2.3522);
+        when(weatherService.postWeather("Paris", 48.8566, 2.3522))
+                .thenReturn("Weather information for Paris at lat: 48.8566, lon: 2.3522");
+        String response = weatherController.postWeather(request);
+        assertEquals("Weather information for Paris at lat: 48.8566, lon: 2.3522", response);
+    }
+}
