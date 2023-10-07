45791d3e37a754ebc2ba801376a8ac6f09a503d4
saeedAliakbary <saeedaliakbari.123@gmail.com>
2023-10-07 09:48:03 +0330

    feat: start project

---
+++ src/main/java/com/relay/apirelay/ApiRelayApplication.java
@@ -0,0 +1,13 @@
+package com.relay.apirelay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRelayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRelayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/relay/apirelay/ApiRelayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.relay.apirelay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRelayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
