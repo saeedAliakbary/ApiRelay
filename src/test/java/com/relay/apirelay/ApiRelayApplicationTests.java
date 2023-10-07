1330001060dc6915b267e0df550699216b280da1
saeedAliakbary <saeedaliakbari.123@gmail.com>
2023-10-07 13:20:47 +0330

    test-ApiRelay: first step of TDD failed process

--- src/test/java/com/relay/apirelay/ApiRelayApplicationTests.java
+++ src/test/java/com/relay/apirelay/ApiRelayApplicationTests.java
@@ -3,11 +3,24 @@ package com.relay.apirelay;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class ApiRelayApplicationTests {
 
+
     @Test
-    void contextLoads() {
+    void testGetWeather() {
+        WeatherService weatherService = new WeatherService();
+        String weather = weatherService.getWeather("Tehran");
+        assertEquals("Weather information for Tehran", weather);
+    }
+
+    @Test
+    void testPostWeather() {
+        WeatherService weatherService = new WeatherService();
+        String weather = weatherService.postWeather("Paris", 48.8566, 2.3522);
+        assertEquals("Weather information for Paris at lat: 48.8566, lon: 2.3522", weather);
     }
 
 }
